fd9cbc045bf7e28ca35954b6e5fa72146539ed08
thoeschler <thilo.hoeschler@gmx.de>
2023-02-24 16:49:48 +0100

    Rename circle_segment to cylinder_segment

--- Mathematica/math_to_py.py
+++ Mathematica/math_to_py.py
@@ -65,24 +65,24 @@ def bar_magnet_python():
 def magnet_segment_python():
     dir_name = os.path.dirname(os.path.realpath(__file__))
     for c in ("x", "y", "z"):
-        open(dir_name + f"/circle_segment/circle_segment_{c}.py.raw", "w").close()
+        open(dir_name + f"/cylinder_segment/cylinder_segment_{c}.py.raw", "w").close()
     # x-component
     for fname in ("x_t1.1.txt", "x_t1.2.txt", "x_t2.1.txt", "x_t2.2.txt", "x_t3.txt"):
-        with open(dir_name + f"/circle_segment/{fname}", "r") as f:
+        with open(dir_name + f"/cylinder_segment/{fname}", "r") as f:
             expr = f.read()
-            with open(dir_name + "/circle_segment/circle_segment_x.py.raw", "a+") as f_py:
+            with open(dir_name + "/cylinder_segment/cylinder_segment_x.py.raw", "a+") as f_py:
                 f_py.write(to_python(expr) + "\n")
     # y-component
     for fname in ("y_t1.1.txt", "y_t1.2.txt", "y_t2.1.txt", "y_t2.2.txt", "y_t3.txt"):
-        with open(dir_name + f"/circle_segment/{fname}", "r") as f:
+        with open(dir_name + f"/cylinder_segment/{fname}", "r") as f:
             expr = f.read()
-            with open(dir_name + "/circle_segment/circle_segment_y.py.raw", "a+") as f_py:
+            with open(dir_name + "/cylinder_segment/cylinder_segment_y.py.raw", "a+") as f_py:
                 f_py.write(to_python(expr) + "\n")
     # z-component
     for fname in ("z_t1.txt", "z_t2.txt", "z_t3.1.txt", "z_t3.2.txt"):
-        with open(dir_name + f"/circle_segment/{fname}", "r") as f:
+        with open(dir_name + f"/cylinder_segment/{fname}", "r") as f:
             expr = f.read()
-            with open(dir_name + "/circle_segment/circle_segment_z.py.raw", "a+") as f_py:
+            with open(dir_name + "/cylinder_segment/cylinder_segment_z.py.raw", "a+") as f_py:
                 f_py.write(to_python(expr) + "\n")
 
 
