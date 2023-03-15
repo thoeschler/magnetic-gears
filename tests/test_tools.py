a3185f78abf752bc11739afa912ce3d1f2ceae5b
thoeschler <thilo.hoeschler@gmx.de>
2023-03-15 15:36:04 +0100

    include magnetic potential interpolation

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -25,6 +25,15 @@ def test_ball_magnet():
     mesh = read_mesh(f"{test_dir}/{mesh_fname}")
 
     # interpolate reference field
+    Vm_interpol = interpolate_field(ref_mag.Vm, mesh, cell_type, p_deg, fname=f"{test_dir}/Vm_ball", write_pvd=True)
+    field_name = "Vm"
+    field_file_name = "Vm_ball.h5"
+
+    # write interpolated field to hdf5 file and read it
+    write_hdf5_file(Vm_interpol, mesh, f"{test_dir}/{field_file_name}", field_name)
+    _ = read_hd5f_file(f"{test_dir}/{field_file_name}", field_name, mesh, cell_type, p_deg, vector_valued=False)
+
+    # interpolate reference field
     B_interpol = interpolate_field(ref_mag.B, mesh, cell_type, p_deg, fname=f"{test_dir}/B_ball", write_pvd=True)
     field_name = "B"
     field_file_name = "B_ball.h5"
@@ -38,19 +47,28 @@ def test_bar_magnet():
     domain_radius = 7
     cell_type = "CG"
     p_deg = 1
-    mesh_size_min = 0.05
+    mesh_size_min = 0.3
     mesh_size_max = 1.0
-    
+
     # file names
-    mesh_fname = "reference_mesh.xdmf"
+    mesh_fname = "reference_mesh_bar.xdmf"
 
     # reference magnet
-    ref_mag = BarMagnet(width=1., depth=1., height=5., magnetization_strength=1., \
+    ref_mag = BarMagnet(width=2., depth=1., height=5., magnetization_strength=1., \
                         position_vector=np.zeros(3), rotation_matrix=np.eye(3))
 
     # create mesh, write it to xdmf file and read it
     create_reference_mesh(ref_mag, domain_radius, mesh_size_min, mesh_size_max, fname=f"{test_dir}/{mesh_fname}.xdmf")
     mesh = read_mesh(f"{test_dir}/{mesh_fname}")
+
+    # interpolate reference field
+    Vm_interpol = interpolate_field(ref_mag.Vm, mesh, cell_type, p_deg, fname=f"{test_dir}/Vm_bar", write_pvd=True)
+    field_name = "Vm"
+    field_file_name = "Vm_bar.h5"
+
+    # write interpolated field to hdf5 file and read it
+    write_hdf5_file(Vm_interpol, mesh, f"{test_dir}/{field_file_name}", field_name)
+    _ = read_hd5f_file(f"{test_dir}/{field_file_name}", field_name, mesh, cell_type, p_deg, vector_valued=False)
 
     # interpolate reference field
     B_interpol = interpolate_field(ref_mag.B, mesh, cell_type, p_deg, fname=f"{test_dir}/B_bar", write_pvd=True)
@@ -81,10 +99,10 @@ def test_magnet_segment():
 
 if __name__ == "__main__":
     # create test directory
-    test_dir = os.getcwd() + "/test_dir"
+    test_dir = os.getcwd() + "/test_tools"
     if not os.path.exists(test_dir):
         os.mkdir(test_dir)
-    
+
     test_ball_magnet()
     test_bar_magnet()
     test_magnet_segment()
