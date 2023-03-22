fix sign of magnetization in cylinder segment magnet

source/magnet_classes.py
@@ -586,7 +586,7 @@ class BarMagnet(PermanentAxialMagnet):
 
 class CylinderSegment(PermanentMagnet):
     def __init__(self, radius, width, thickness, alpha, magnetization_strength, position_vector, \
-                 rotation_matrix, magnetization_direction="out"):
+                 rotation_matrix, magnetization_sign=1):
         """
         Radially magnetized CylinderSegment.
 
@@ -601,17 +601,18 @@ class CylinderSegment(PermanentMagnet):
                                           center of mass. Instead, it is the point at dis-
                                           tance Rm with a zero angle (in eigen coordinates).
                                           This can be considered the mid point.
-            magnetization_direction (str, optional): The radial magnetization can be either
-                                                     pointing "in" (negative radial direction)
-                                                     or "out" (positive radial direction).
-                                                     Defaults to "out".
+            magnetization_sign (int, optional): The radial magnetization can be either
+                                                     pointing in negative radial direction
+                                                     (-1) or in positive radial direction (1).
+                                                     Defaults to 1.
         """
         super().__init__(type_classifier='CylinderSegment',
                          position_vector=position_vector,
                          magnetization_strength=magnetization_strength,
                          rotation_matrix=rotation_matrix
                          )
-        assert magnetization_direction in ("out", "in")
+        assert magnetization_sign in (1, -1)
+        self._mag_sign = magnetization_sign
         self._Rm = radius
         self._width = width
         self._thickness = thickness
@@ -636,6 +637,10 @@ class CylinderSegment(PermanentMagnet):
     @property
     def alpha(self):
         return self._alpha
+
+    @property
+    def mag_sign(self):
+        return self._mag_sign
 
     def is_inside(self, x0):
         """Check if point x0 is inside the magnet's domain.
@@ -714,7 +719,7 @@ class CylinderSegment(PermanentMagnet):
         """
         _, y, z = x_eigen
         rho = np.sqrt(y ** 2 + z ** 2)
-        return np.array([0., y / rho, z / rho])
+        return self.mag_sign * np.array([0., y / rho, z / rho])
 
     def M(self, x0):
         """

source/magnetic_gear_classes.py
@@ -515,9 +515,9 @@ class SegmentGear(MagneticGear):
             CylinderSegment: The reference magnet.
         """
         return CylinderSegment(radius=self.R / self.scale_parameter, width=self.w / self.scale_parameter, \
-                             thickness=self.t / self.scale_parameter, alpha=self.alpha, \
+                               thickness=self.t / self.scale_parameter, alpha=self.alpha, \
                                 magnetization_strength=1.0, position_vector=np.zeros(3), \
-                                    rotation_matrix=np.eye(3))
+                                    rotation_matrix=np.eye(3), magnetization_sign=1)
 
     def create_magnets(self, magnetization_strength):
         """Create magnets, given some magnetization strength.
@@ -532,14 +532,14 @@ class SegmentGear(MagneticGear):
         for k in range(self.n):
             if k % 2 == 0:
                 # magnetization pointing outward
-                magnetization_direction = "out"
+                magnetization_sign = 1
             else:
                 # magnetization pointing inward
-                magnetization_direction = "in"
+                magnetization_sign = -1
             Q = get_rot(2 * np.pi / self.n * k)
             x_M = self.x_M + Q.dot(np.array([0., self.R, 0.]))
             self._magnets.append(CylinderSegment(radius=self.R, width=self.w, thickness=self.t, \
                                                  alpha=self.alpha, magnetization_strength=magnetization_strength, \
                                                     position_vector=x_M, rotation_matrix=Q, \
-                                                        magnetization_direction=magnetization_direction))
+                                                        magnetization_sign=magnetization_sign))
         print("Done.")

spur_gears/spur_gears_problem.py
@@ -408,7 +408,7 @@ class SpurGearsProblem:
 
         self.segment_mesh, _, _ = cylinder_segment_mesh(Ri=Ri, Ro=Ro, t=t, angle=angle, x_M_ref=self.lg.x_M, \
                                                         x_axis=x_axis, fname=ref_path + "/reference_segment", \
-                                                        pad=False, mesh_size=mesh_size, write_to_pvd=True)
+                                                        pad=True, mesh_size=mesh_size, write_to_pvd=True)
 
     def interpolate_to_reference_segment(self, p_deg=2, interpolate="twice", use_Vm=True):
         """
@@ -482,8 +482,12 @@ class SpurGearsProblem:
         for mag in gear.magnets:
             print("Interpolating field of magnet...", end="")
             if use_ref_field:
+                scale = gear.scale_parameter
+                if not isinstance(mag, PermanentAxialMagnet):
+                    scale *= mag.mag_sign * gear.reference_magnet().mag_sign
+
                 interpol_field = self._interpolate_field_magnet(mag, ref_field, field_name, ref_mesh, \
-                    mesh, cell_type, p_deg, scale=gear.scale_parameter)
+                    mesh, cell_type, p_deg, scale=scale)
                 assert interpol_field.function_space().mesh() is mesh
             else:
                 if field_name == "B":
@@ -526,6 +530,9 @@ class SpurGearsProblem:
             V_ref = dlf.VectorFunctionSpace(self._B_reference_mesh_copy, cell_type, p_deg)
             ref_field_copy = dlf.Function(V_ref, ref_field.vector().copy())
             V = dlf.VectorFunctionSpace(mesh, cell_type, p_deg)
+            # scale field sign
+            if not np.isclose(scale, 1.0):
+                ref_field_copy.vector()[:] *= np.sign(scale)
             # rotate reference field
             rotate_vector_field(ref_field_copy, magnet.Q)
         elif field_name == "Vm":
@@ -537,12 +544,13 @@ class SpurGearsProblem:
             ref_field_copy = dlf.Function(V_ref, ref_field.vector().copy())
             if not np.isclose(scale, 1.0):
                 ref_field_copy.vector()[:] *= scale
+            print("SCALING VALUE:", scale)
             V = dlf.FunctionSpace(mesh, cell_type, p_deg)
         else:
             raise RuntimeError()
 
         # scale, rotate and shift reference mesh according to magnet placement
-        ref_mesh_copy.scale(scale)
+        ref_mesh_copy.scale(abs(scale))
         # rotate first, then shift!
         coords = np.array(ref_mesh_copy.coordinates())
         ref_mesh_copy.coordinates()[:] = magnet.Q.dot(coords.T).T