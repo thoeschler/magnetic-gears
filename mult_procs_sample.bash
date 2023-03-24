a9f28f7205a7631915b11c7615c74f1595e0c439
thoeschler <thilo.hoeschler@gmx.de>
2023-03-24 17:38:44 +0100

    pass magnet type as parameter

--- mult_procs_sample.bash
+++ mult_procs_sample.bash
@@ -2,7 +2,8 @@
 
 # number of processes
 nb_procs=$1
+magnet=$2
 
 for ((proc_num=1;proc_num<=$nb_procs;proc_num++)); do
-    ./sample_torque.bash $proc_num $nb_procs &
+    ./sample_torque.bash $proc_num $nb_procs $magnet &
 done
--- sample_torque.bash
+++ sample_torque.bash
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-: '
-for nb_it in {0..199}; do
-    echo -e $'\n'"ITERATION:" $(($nb_it + 1)) "\n"
-    python3 spur_gears/sample_torque.py "ball" $nb_it
-done
-'
-: '
-for nb_it in {0..200}; do
-    echo -e $'\n'"ITERATION" $(($nb_it + 1))
-    python3 spur_gears/sample_torque.py "bar" $nb_it
-done
-'
 proc_num=$1
 nb_procs=$2
+magnet=$3
 
-for ((par_nb=$proc_num;par_nb<=8;par_nb+=nb_procs)); do
-    echo -e $'\n'"ITERATION:" $(($par_nb + 1))
-    python3 sample_torque.py "cylinder_segment" $par_nb
+for ((par_nb=$proc_num;par_nb<=1;par_nb+=nb_procs)); do
+    echo -e $'\n'"ITERATION:" $(($par_nb-1))
+    python3 sample_torque.py $magnet $(($par_nb-1))
 done
