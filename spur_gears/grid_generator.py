113dda106766d56463a4dd0d9bee9736092f2340
thoeschler <thilo.hoeschler@gmx.de>
2023-03-29 08:57:15 +0200

    reference segment design with start and stop angle

--- spur_gears/grid_generator.py
+++ spur_gears/grid_generator.py
@@ -4,7 +4,7 @@ import numpy as np
 from source.tools.mesh_tools import generate_mesh_with_markers
 
 
-def add_cylinder_segment(model, Ri, Ro, t, angle, x_M, x_axis):
+def add_cylinder_segment(model, Ri, Ro, t, angle_start, angle_stop, x_M, x_axis):
     """
     Add cylinder segment to gmsh model.
 
@@ -13,13 +13,15 @@ def add_cylinder_segment(model, Ri, Ro, t, angle, x_M, x_axis):
         Ri (float): Inner radius.
         Ro (float): Outer radius.
         t (float): Segment thickness.
-        angle (float): Opening angle.
+        angle_start (float): Starting angle.
+        angle_stop (float): Final angle..
         x_M (np.ndarray): Reference/mid point. 
         x_axis (np.ndarray): Starting axis for angle.
 
     Returns:
         int: Segment tag.
     """
+    assert angle_stop > angle_start
     if np.isclose(Ri, 0.):
         Ri = Ro / 30.  # cannot be zero, otherwise gmsh cannot create any elements
     # add first surface
@@ -41,13 +43,13 @@ def add_cylinder_segment(model, Ri, Ro, t, angle, x_M, x_axis):
 
     # add wire
     r = (Ri + Ro) / 2
-    curve = model.occ.addCircle(*x_M, r=r, angle1=-angle / 2, angle2=angle / 2, zAxis=np.array([1., 0., 0.]), xAxis=x_axis)
+    curve = model.occ.addCircle(*x_M, r=r, angle1=angle_start, angle2=angle_stop, zAxis=np.array([1., 0., 0.]), xAxis=x_axis)
     wire = model.occ.addWire([curve])
     segment = model.occ.addPipe([(2, surf)], wire)[0][1]
 
     return segment
 
-def cylinder_segment_mesh(Ri, Ro, t, angle, x_M_ref, x_axis, fname, mesh_size, pad=True, write_to_pvd=False, \
+def cylinder_segment_mesh(Ri, Ro, t, angle_start, angle_stop, x_M_ref, x_axis, fname, mesh_size, pad=True, write_to_pvd=False, \
     verbose=False):
     """
     Create a mesh of a cylinder segment
@@ -56,7 +58,8 @@ def cylinder_segment_mesh(Ri, Ro, t, angle, x_M_ref, x_axis, fname, mesh_size, p
         Ri (float): Inner radius.
         Ro (float): Outer radius
         t (float): Thickness.
-        angle (float): Opening angle.
+        angle_start (float): Starting angle.
+        angle_stop (float): Final angle.
         x_M_ref (np.ndarray): Reference/mid point.
         x_axis (np.ndarray): Starting axis for angle.
         fname (str): File name.
@@ -67,16 +70,19 @@ def cylinder_segment_mesh(Ri, Ro, t, angle, x_M_ref, x_axis, fname, mesh_size, p
     Returns:
         tuple: Mesh, cell marker and facet marker.
     """
+    assert angle_stop > angle_start
     if pad:
         padding = Ro / 60.
         Ri -= padding
         Ro += padding
         t += 2 * padding
-        angle += angle / 60.
+        angle_start -= (angle_stop - angle_start) / 60.
+        angle_stop += (angle_stop - angle_start) / 60.
 
     # make sure angle is at most 2 pi
-    if angle > 2 * np.pi:
-        angle = 2 * np.pi
+    if angle_stop - angle_start > 2 * np.pi:
+        angle_start = 0.
+        angle_stop = 2 * np.pi
 
     assert len(x_M_ref) == 3
     # make sure x_axis is normalized
@@ -87,7 +93,8 @@ def cylinder_segment_mesh(Ri, Ro, t, angle, x_M_ref, x_axis, fname, mesh_size, p
         gmsh.option.setNumber("General.Terminal", 0)
 
     model = gmsh.model()
-    segment = add_cylinder_segment(model, Ri, Ro, t, angle=angle, x_M=x_M_ref, x_axis=x_axis)
+    segment = add_cylinder_segment(model, Ri, Ro, t, angle_start=angle_start, \
+                                   angle_stop=angle_stop, x_M=x_M_ref, x_axis=x_axis)
     model.occ.synchronize()
 
     segment_boundary = model.getBoundary([(3, segment)], oriented=False)[0][1]
