c10146d790990179fea1f80d98ea3dbfc6b9d6ef
thoeschler <thilo.hoeschler@gmx.de>
2023-04-13 14:17:50 +0200

    add 2d field solution and parameter study

---
+++ parameter_study_source/parameter_study_gr_R_p/parameter_study_gr_R_p_2D.py
@@ -0,0 +1,114 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+import os
+import itertools as it
+from source.magnetic_gear_classes import SegmentGear
+from spur_gears.spur_gears_problem import SpurGearsProblem
+from source.cylinder_segment_2D import force_torque_cylinder_segment_2D
+
+
+def sample(spur_gears: SpurGearsProblem, data_dir=".", n_iterations=20):
+    assert isinstance(spur_gears.gear_1, SegmentGear)
+    assert isinstance(spur_gears.gear_2, SegmentGear)
+    angle_1_lim = [0, np.pi / spur_gears.gear_1.p]
+    angle_2_lim = [- np.pi / spur_gears.gear_2.p, np.pi / spur_gears.gear_2.p]
+    angles_1 = np.linspace(*angle_1_lim, num=n_iterations, endpoint=True)
+    angles_2 = np.linspace(*angle_2_lim, num=n_iterations, endpoint=True)
+
+    tau_12_values = np.zeros((n_iterations, n_iterations))
+    tau_21_values = np.zeros((n_iterations, n_iterations))
+    for i, angle_1 in enumerate(angles_1):
+        for j, angle_2 in enumerate(angles_2):
+            # compute torque
+
+            f_12, tau_12 = force_torque_cylinder_segment_2D(w2=spur_gears.gear_2.width, \
+                                                t1=spur_gears.gear_1.t, \
+                                                t2=spur_gears.gear_2.t, \
+                                                p1=spur_gears.gear_1.p, \
+                                                p2=spur_gears.gear_2.p, \
+                                                Rm1=spur_gears.gear_1.R, \
+                                                Rm2=spur_gears.gear_2.R, \
+                                                phi_1=angle_1, \
+                                                phi_2=angle_2, \
+                                                D = spur_gears.D, \
+                                                nb_terms=50)
+
+            # save torque values
+            f_12_padded = np.array([0., f_12[0], f_12[1]])
+            tau_21 = np.cross(spur_gears.gear_1.x_M - spur_gears.gear_2.x_M, f_12_padded)[0] - tau_12
+            tau_12_values[i, j] = tau_12
+            tau_21_values[i, j] = tau_21
+
+            # update csv files
+            pd.DataFrame(tau_12_values, index=angles_1, columns=angles_2).to_csv(os.path.join(data_dir, "tau_12.csv"))
+            pd.DataFrame(tau_21_values, index=angles_1, columns=angles_2).to_csv(os.path.join(data_dir, "tau_21.csv"))
+
+def sample_cylinder_segment(par_number, n_iterations=20):
+    t1 = 1.0
+    R1 = 10.
+    d_ref = 0.1
+    w_ref = 3.
+    # create sample directory
+    sample_dir = "sample_cylinder_segment_gear_2D"
+    if not os.path.exists(sample_dir):
+        os.mkdir(sample_dir)
+
+    # parameters
+    gear_ratio_values = np.array([1.0, 1.4, 2.0, 2.4])
+    R_ref_values = np.array([6., 10., 20.])
+    scaling_factors = R1 / R_ref_values
+
+    w1_values = w_ref * scaling_factors
+    p1_values = list(range(6, 72, 2))
+    par_list = list(it.product(gear_ratio_values, w1_values, p1_values))
+    par = par_list[par_number]
+    gear_ratio, w1, p1 = par
+    w2 = w1
+
+    # compute rest of values
+    R2 = R1 * gear_ratio
+    p2 = p1 * gear_ratio
+    if not p2.is_integer():
+        return
+    if int(p2) % 2 != 0:
+        return
+    p2 = int(p2)
+    # scale d accordingly
+    d = d_ref * w1 / w_ref
+    # compute t2
+    t2 = R2 / R1 * t1
+
+    # create directory
+    gear_ratio_dir = f"gear_ratio_{str(gear_ratio).replace('.', 'p')}"
+    width_dir = f"w1_{str(w1).replace('.', 'p')}"
+    pole_nb_dir = f"p1_{str(p1).replace('.', 'p')}"
+    data_dir = os.path.join(gear_ratio_dir, width_dir, pole_nb_dir)
+    target_dir = os.path.join(sample_dir, data_dir)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    # create directory
+    gear_ratio_dir = f"gear_ratio_{str(gear_ratio).replace('.', 'p')}"
+    width_dir = f"w1_{str(w1).replace('.', 'p')}"
+    pole_nb_dir = f"p1_{str(p1).replace('.', 'p')}"
+    data_dir = os.path.join(gear_ratio_dir, width_dir, pole_nb_dir)
+    target_dir = os.path.join(sample_dir, data_dir)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    gear_1 = SegmentGear(p1, R1 - t1 / 2, w1, t1, np.zeros(3))
+    gear_2 = SegmentGear(p2, R2 - t2 / 2, w2, t2, np.array([0., 1., 0.]))
+
+    gear_1.create_magnets(1.0)
+    gear_2.create_magnets(1.0)
+    D = gear_1.outer_radius + gear_2.outer_radius + d
+
+    spur_gears = SpurGearsProblem(gear_1, gear_2, D)
+
+    sample(spur_gears, data_dir=target_dir, n_iterations=n_iterations)
+
+
+if __name__ == "__main__":
+    for i in range(500):
+        sample_cylinder_segment(i, n_iterations=5)
---
+++ source/cylinder_segment_2D.py
@@ -0,0 +1,93 @@
+import numpy as np
+import scipy as sp
+
+
+"""
+In this file the 2d field of a hollow cylinder with p pole pairs
+is implemented. The field solution is taken from this article
+https://iopscience.iop.org/article/10.1088/0022-3727/33/1/305.
+"""
+
+def integrand_force(theta_edge, phi_1, D, p, Ri, Ro, nb_terms):
+    func = lambda r: (
+        - Bz_cylinder_segment_2D(r, theta_edge, phi_1, D, p, Ri, Ro, nb_terms), \
+        By_cylinder_segment_2D(r, theta_edge, phi_1, D, p, Ri, Ro, nb_terms)
+        )
+    return func
+
+def integrand_torque(theta_edge, phi_1, D, p, Ri, Ro, nb_terms):
+    func = lambda r: r * (np.cos(theta_edge) * By_cylinder_segment_2D(r, theta_edge, phi_1, D, p, Ri, Ro, nb_terms) + \
+        np.sin(theta_edge) * Bz_cylinder_segment_2D(r, theta_edge, phi_1, D, p, Ri, Ro, nb_terms))
+    return func
+
+def integrate(integrand_func, theta_2_edge, phi_1, D, p, Ri1, Ro1, Ri2, Ro2, nb_terms):
+    assert callable(integrand_func)
+    integrand = integrand_func(theta_2_edge, phi_1, D, p, Ri1, Ro1, nb_terms)
+    if isinstance(integrand(0), (tuple, list)):
+        result = []
+        for i in range(len(integrand(0))):
+            integrand_comp = lambda r: integrand(r)[i]
+            result_comp, _ = sp.integrate.quad(integrand_comp, Ri2, Ro2)
+            result.append(result_comp)
+        return np.array(result)
+    result, _ = sp.integrate.quad(integrand, Ri2, Ro2)
+    return result
+
+def force_torque_cylinder_segment_2D(w2, t1, t2, p1, p2, Rm1, Rm2, phi_1, phi_2, D, nb_terms=40):
+    """
+    Compute torque from gear 1 on gear 2.
+    """
+    # sum over all magnets
+    f = np.zeros(2)
+    tau = 0.
+    Ri1 = Rm1 - t1 / 2
+    Ro1 = Rm1 + t1 / 2
+    Ri2 = Rm2 - t2 / 2
+    Ro2 = Rm2 + t2 / 2
+    for n2 in range(p2):
+        theta_2_edge = phi_2 + np.pi / p2 * (1 + 2 * n2)
+
+        f += (-1) ** n2 * integrate(integrand_force, theta_2_edge, phi_1, D, p1, Ri1, Ro1, Ri2, Ro2, nb_terms)
+        tau += (-1) ** n2 * integrate(integrand_torque, theta_2_edge, phi_1, D, p1, Ri1, Ro1, Ri2, Ro2, nb_terms)
+
+    return 2 * w2 * f, 2 * w2 * tau
+
+def By_cylinder_segment_2D(r, theta, phi_1, D, p, Ri, Ro, nb_terms):
+    rp = np.sqrt(r ** 2 + 2 * r * D * np.cos(theta) + D ** 2)
+    thetap = np.arctan2(r * np.sin(theta), r * np.cos(theta) + D)
+    By = Br_cylinder_segment_2D_eigen(rp, thetap - phi_1, p, Ri, Ro, nb_terms) * np.cos(thetap) - \
+        Btheta_cylinder_segment_2D_eigen(rp, thetap - phi_1, p, Ri, Ro, nb_terms) * np.sin(thetap)
+    return By
+
+def Bz_cylinder_segment_2D(r, theta, phi_1, D, p, Ri, Ro, nb_terms):
+    rp = np.sqrt(r ** 2 + 2 * r * D * np.cos(theta) + D ** 2)
+    thetap = np.arctan2(r * np.sin(theta), r * np.cos(theta) + D)
+    Bz = Br_cylinder_segment_2D_eigen(rp, thetap - phi_1, p, Ri, Ro, nb_terms) * np.sin(thetap) + \
+        Btheta_cylinder_segment_2D_eigen(rp, thetap - phi_1, p, Ri, Ro, nb_terms) * np.cos(thetap)
+    return Bz
+
+def Br_cylinder_segment_2D_eigen(rp, thetap, p, Ri, Ro, nb_terms):
+    k = np.arange(nb_terms) + 1
+    Mk = M(k, p)
+    return np.sum(k * rp ** (- (k + 1)) * U2(Mk, Ri, Ro, k) * np.cos(k * thetap))
+
+def Btheta_cylinder_segment_2D_eigen(r, theta, p, Ri, Ro, nb_terms):
+    k = np.arange(nb_terms) + 1
+    Mk = M(k, p)
+    return np.sum(k * r ** (- (k + 1)) * U2(Mk, Ri, Ro, k) * np.sin(k * theta))
+
+def U2(M, Ri, Ro, k):
+    return - H2(M, Ri, Ro, k) / 4
+
+def H2(M, Ri, Ro, k):
+    return Ro ** (k + 1) * (
+        2 / Ro * M / k / (k + 1) * (Ro - (Ri / Ro) ** k * Ri) - \
+        2 * M / k + 2 * M / k * (Ri / Ro) ** (k + 1)
+    )
+
+def M(k, p):
+    assert isinstance(p, int)
+    k = np.atleast_1d(k)[np.newaxis]
+    m = np.arange(p) + 1
+    m = m[:, np.newaxis]
+    return np.sum(1 / k / np.pi * (-1) ** m * (np.sin((2 * m - 1) / p * k * np.pi) - np.sin((2 * m + 1) / p * k * np.pi)), axis=0)
