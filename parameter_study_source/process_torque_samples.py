0e363d3dac5c165a59b18bc29947fadd75ecd628
Thilo Höschler <thilo.hoeschler@gmx.de>
2024-05-03 14:58:37 +0200

    add optimal magnet numbers func

--- parameter_study_source/process_torque_samples.py
+++ parameter_study_source/process_torque_samples.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 
 
@@ -41,7 +40,7 @@ def write_file(pole_numbers, torque_values, fname):
             f.write(f"{p} {tau} \n")
 
 def process_parameter_study_gr_R_p():
-    gear_ratio_names = ["gear_ratio_1p0", "gear_ratio_1p4", "gear_ratio_2p0"]#, "gear_ratio_2p4"]
+    gear_ratio_names = ["gear_ratio_1p0", "gear_ratio_1p4", "gear_ratio_2p0", "gear_ratio_2p4"]
     w1_names = ["w1_1p5", "w1_3p0", "w1_5p0"]
     # In the computation the lengths are scaled differently. In order to get equal values for the distance
     # d and the width as well as different radii R we need to rescale the torque values by a length factor
@@ -53,7 +52,7 @@ def process_parameter_study_gr_R_p():
     for gear_ratio in gear_ratio_names:
         for w1, length_factor in zip(w1_names, length_factors):
             # set directory names
-            source_dir_name = os.path.join("/home/thilo/Dropbox/magnetic gears/python/parameter_study_gr_R_p_cylinder_segment", gear_ratio, w1)
+            source_dir_name = os.path.join("/home/thilo/Dropbox/magnetic gears/python/parameter_study_gr_R_p_cylinder_segment_2D", gear_ratio, w1)
             target_dir_name = os.path.join(source_dir_name)
 
             # load torque and pole number values
@@ -84,7 +83,35 @@ def process_parameter_study_a_p():
         write_file(p_values, tau_12_values, os.path.join(target_dir_name, "tau_12.csv"))
         write_file(p_values, tau_21_values, os.path.join(target_dir_name, "tau_21.csv"))
 
+def compare_torques():
+    gear_ratio_names = ["gear_ratio_1p0", "gear_ratio_1p4", "gear_ratio_2p0", "gear_ratio_2p4"]
+    w1_names = ["w1_1p5", "w1_3p0", "w1_5p0"]
+    source_dir_name = os.path.join("/home/thilo/Dropbox/magnetic gears/python/parameter_study_gr_R_p_cylinder_segment")
+    for gear_ratio in gear_ratio_names:
+        for w1 in w1_names:
+            # set directory names
+            target_dir_name = os.path.join(source_dir_name, gear_ratio, w1)
+            tau_12 = pd.read_csv(os.path.join(target_dir_name, "tau_12.csv"), sep='\\s+', index_col=0).to_numpy(dtype=float)
+            tau_21 = pd.read_csv(os.path.join(target_dir_name, "tau_21.csv"), sep='\\s+', index_col=0).to_numpy(dtype=float)
+            print(tau_12 / tau_21)
+
+def optimal_magnet_numbers():
+    gear_ratio_names = ["gear_ratio_1p0", "gear_ratio_1p4", "gear_ratio_2p0", "gear_ratio_2p4"]
+    w1_names = ["w1_1p5", "w1_3p0", "w1_5p0"]
+    source_dir_name = os.path.join("/home/thilo/Dropbox/magnetic gears/python/parameter_study_gr_R_p_cylinder_segment")
+
+    p_opt = np.empty((len(gear_ratio_names), len(w1_names)), dtype=int)
+    for row, gear_ratio in enumerate(gear_ratio_names):
+        for col, w1 in enumerate(w1_names):
+            # set directory names
+            target_dir_name = os.path.join(source_dir_name, gear_ratio, w1)
+            tau_12 = pd.read_csv(os.path.join(target_dir_name, "tau_12.csv"), sep='\\s+').to_numpy(dtype=float)
+            p_opt[row, col] = tau_12[tau_12[:, 1].argmax(), 0]
+    pd.DataFrame(p_opt, dtype=float, index=[1.0, 1.4, 2.0, 2.4], columns=None).to_csv(
+        os.path.join(source_dir_name, "p_opt.csv"), columns=None, header=None, sep=" ")
 
 if __name__ == "__main__":
-    #process_parameter_study_a_p()
+    process_parameter_study_a_p()
     process_parameter_study_gr_R_p()
+    compare_torques()
+    optimal_magnet_numbers()
