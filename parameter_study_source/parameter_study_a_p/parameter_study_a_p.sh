daac4aba4184294c5cbfe1fea0ba04b9c378e790
Thilo Höschler <thilo.hoeschler@gmx.de>
2024-05-03 14:50:07 +0200

    write iteration number to log file

--- parameter_study_source/parameter_study_a_p/parameter_study_a_p.sh
+++ parameter_study_source/parameter_study_a_p/parameter_study_a_p.sh
@@ -9,6 +9,6 @@ export PYTHONPATH=.
 n_procs=$1
 open_sem $n_procs
 for par_nb in {1..114}; do
-    echo "ITERATION" $((par_nb-1))
+    echo "ITERATION" $((par_nb-1))>>"$DIRNAME/log.txt"
     run_with_lock python3 parameter_study_source/parameter_study_a_p/parameter_study_a_p.py $(( par_nb-1 )) 2>>"$DIRNAME/log.txt"
 done
--- parameter_study_source/parameter_study_gr_R_p/parameter_study_gr_R_p.sh
+++ parameter_study_source/parameter_study_gr_R_p/parameter_study_gr_R_p.sh
@@ -10,6 +10,6 @@ magnet=$1
 n_procs=$2
 open_sem $n_procs
 for par_nb in {17..400}; do
-    echo "ITERATION" $((par_nb-1))
+    echo "ITERATION" $((par_nb-1))>>"$DIRNAME/log.txt"
     run_with_lock python3 parameter_study_source/parameter_study_gr_R_p/parameter_study_gr_R_p.py $magnet $(( par_nb-1 )) 2>>"$DIRNAME/log.txt"
 done
